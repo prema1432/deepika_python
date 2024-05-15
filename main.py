ae71ebc70646b98e7a616b0f5a90267163ace86e
Premanath Talamarla <talamarlapremanath@gmail.com>
2024-05-15 08:59:20 +0530

    init

---
+++ main.py
@@ -0,0 +1,65 @@
+# virtual environment:
+
+# python - libraries, numpy,pandas,-libraries==pandas==2.00,pandas==
+
+# .py = extenstion
+# for comments
+
+# Identifiers:
+a = 10
+print("aaa  value", a)
+# variable
+# class name
+# function name
+
+nnn = ""
+
+
+class deepiks:
+    pass
+
+
+def deep():
+    pass
+
+
+# class == group of functyion == def == function, class ==
+
+# alphabetics-number-__,_
+# dont start with number
+# dont add special
+# -- =strong pvt variable
+# - private variable
+# case sensitive
+a = "20"
+print("a : ", a)
+print("datatype of a : ", type(a))
+print("id of a : ", id(a))
+
+a
+print("A : ", A)
+print("datatype of A : ", type(A))
+print("id of A : ", id(A))
+
+# ='',"",""""""
+
+# a :  20
+# datatype of a :  <class 'int'>
+# id of a :  4384719696
+# A :  34
+# datatype of A :  <class 'int'>
+# id of A :  4384720144
+
+
+
+# print()
+# type()
+# id()
+
+# ""= string
+# 22= int
+# 1.45 = float
+
+# __mysalary = "opo"
+# print("12a : ", __mysalary)
+# print("hai Good Morning")
